ee3a535780ab1eecec85ec79d48581be82474277
IuriMagalhaesSilva <iuri_magalhaessilva@hotmail.com>
2022-10-18 23:09:46 -0300

    Merge

--- Complains/src/main/java/com/fiap/hackathon/complains/config/DynamoDbConfig.java
+++ Complains/src/main/java/com/fiap/hackathon/complains/config/DynamoDbConfig.java
@@ -1,22 +1,15 @@
 package com.fiap.hackathon.complains.config;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
-import org.springframework.context.annotation.Primary;
 import org.springframework.util.StringUtils;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 
 
 @Configuration
